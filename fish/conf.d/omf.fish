9ba8505ac7c7f9c399758ef00d3a751213977b2c
alalfakawma <alalfakawma@live.com>
2021-05-04 21:00:01 +0530

    Dotfiles Updated at: Tue May  4 09:00:01 PM IST 2021

--- fish/conf.d/omf.fish
+++ fish/conf.d/omf.fish
@@ -7,7 +7,7 @@ set -q XDG_DATA_HOME
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
 
-# Make vim default editor
+# Make nvim default editor
 set EDITOR /usr/bin/nvim
 
 # Make firefox default browser
